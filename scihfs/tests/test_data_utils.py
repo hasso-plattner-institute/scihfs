ef16d832fc5a6263785f1ba3342c52e6eeaa71f7
jp-sachs <jps@mailbox.org>
2025-03-14 16:37:40 +0000

    Removed experiments-related functions from tests and updated flake8 hook

--- scihfs/tests/test_data_utils.py
+++ scihfs/tests/test_data_utils.py
@@ -1,33 +1,6 @@
-import os
-from pathlib import Path
-
 import pytest
 
-from scihfs.data_utils import create_mapping_columns_to_nodes, load_data, process_data
-
-
-def remove_created_files():
-    dirname = os.path.dirname(__file__)
-    data_file = os.path.join(
-        dirname, Path("../data/sport_tweets_train_with_hierarchy_testing1.csv")
-    )
-    hierarchy_file = os.path.join(
-        dirname, Path("../data/sport_tweets_train_hierarchy_testing1.pickle")
-    )
-    os.remove(data_file)
-    os.remove(hierarchy_file)
-
-
-def test_process_data_no_error():
-    process_data(test_version=True)
-    remove_created_files()
-
-
-def test_load_data():
-    X, labels, hierarchy = load_data(test_version=True)
-    mapping = create_mapping_columns_to_nodes(X, hierarchy)
-    assert len(mapping) == 269
-    assert len(X) == labels.shape[0]
+from scihfs.data_utils import create_mapping_columns_to_nodes
 
 
 @pytest.mark.parametrize(

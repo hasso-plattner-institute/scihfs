9243849f0ee6c6f6d9539e96c74f955e7bdfa0d3
jp-sachs <jps@mailbox.org>
2026-07-30 06:57:04 +0000

    Basic adoption of lazy selector design for HieAODE, tbc

--- scihfs/tests/test_hie_aode.py
+++ scihfs/tests/test_hie_aode.py
@@ -12,14 +12,14 @@ from scihfs.selectors import HieAODE
 def test_hie_aode(lazy_data2):
     small_DAG, train_x_data, train_y_data, test_x_data, _ = lazy_data2
     selector = HieAODE(hierarchy=small_DAG)
-    selector.fit_selector(X_train=train_x_data, y_train=train_y_data, X_test=test_x_data)
-    _ = selector.select_and_predict(predict=True, saveFeatures=True)
+    selector.fit(train_x_data, train_y_data)
+    _ = selector.predict(test_x_data)
 
 
 def test_calculate_dependency_ascendant_class(lazy_data2):
-    small_DAG, train_x_data, train_y_data, test_x_data, _ = lazy_data2
+    small_DAG, train_x_data, train_y_data, _, _ = lazy_data2
     selector = HieAODE(hierarchy=small_DAG)
-    selector.fit_selector(X_train=train_x_data, y_train=train_y_data, X_test=test_x_data)
+    selector.fit(train_x_data, train_y_data)
     feature_idx = 2
     expected = np.full((selector.n_features_in_, selector.n_classes_, 2), -1)
     expected[0][0][0] = 0.0
